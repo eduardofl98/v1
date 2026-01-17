36257e7e32af472bab18760c39c96f6dd6236df3
eduardofl98 <efloresv98@gmail.com>
2026-01-17 22:08:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+st.title("PRUEBA EN LA NUBE")
+st.write("Si ves esto, ya estamos ejecutando el archivo correcto.")
+
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
