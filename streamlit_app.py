0281bfb0c9d24228578f3b5e9153aa9e0db80cbd
eduardofl98 <efloresv98@gmail.com>
2026-01-17 22:19:09 +0100

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ import streamlit as st
 # -----------------------------
 # CONFIG
 # -----------------------------
-APP_TITLE = "AI Training & Loss Aversion (Lottery Experiment) prueb"
+APP_TITLE = "AI Training & Loss Aversion (Lottery Experiment)"
 N_PRE = 3
 N_TRAIN = 5
 N_POST = 2
